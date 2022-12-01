eef3305c948850e974c286f5e8cac44163271657
GURJYOT BHULLAR <gjbhullar16@gmail.com>
2022-12-01 20:22:33 +0530

    Fix bugs

--- algo.py
+++ algo.py
@@ -1,7 +1,13 @@
-# import sys    
+import pygame as py, colors, main as m, sys
 
 mini= 1000000009
 minimumpath=[]
+
+# globals
+city_states = {}
+cities = {}
+adj = []
+edges = []
 
 
 def cost(nodes,costmatrix):
@@ -11,6 +17,13 @@ def cost(nodes,costmatrix):
     cst+=costmatrix[nodes[len(nodes)-1]][nodes[0]]
     return cst
 
+def init_globals(x, y, z):
+    global city_states
+    global cities
+    global adj
+    city_states = x
+    cities = y
+    adj = z
 
 def permute(start,end,nodes,costmatrix):
     global mini
@@ -20,34 +33,39 @@ def permute(start,end,nodes,costmatrix):
         if fps<mini:
             mini=fps
             minimumpath=nodes[:]
-            # print(minimumpath)
     else:
         for i in range(start,end):
             nodes[start],nodes[i]=nodes[i],nodes[start]
+            # city_states[nodes[start]] = colors.LIGHT_PURPLE
+            # m.update_screen(cities, city_states)
+            city_states[nodes[start]] = colors.GREEN
+            m.update_screen(cities, city_states, 1)
             permute(start+1,end,nodes,costmatrix)
             nodes[start],nodes[i]=nodes[i],nodes[start]
+            city_states[nodes[start]] = colors.RED
+            m.update_screen(cities, city_states, 1)
             
 
 
 
-def main():
-    global mini
-    global minimumpath
-    # print(sys.maxint)
-    n=4
-    nodes= [x for x in range(0,n)]
-    costmatrix=[[ 0, 10, 15, 20 ],
-                       [ 10, 0, 35, 25 ],
-                       [ 15, 35, 0, 30 ],
-                       [ 20, 25, 30, 0 ]]
-    permute(0,n,nodes,costmatrix)
-    print(mini)
-    for i in range(n):
-        minimumpath[i]=minimumpath[i]+1
+# def main():
+#     global mini
+#     global minimumpath
+#     # print(sys.maxint)
+#     n=4
+#     nodes= [x for x in range(0,n)]
+#     costmatrix=[[ 0, 10, 15, 20 ],
+#                        [ 10, 0, 35, 25 ],
+#                        [ 15, 35, 0, 30 ],
+#                        [ 20, 25, 30, 0 ]]
+#     permute(0,n,nodes,costmatrix)
+#     print(mini)
+#     for i in range(n):
+#         minimumpath[i]=minimumpath[i]+1
     
 
 
 
 
-if __name__=="__main__":
-    main()
+# if __name__=="__main__":
+#     main()
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import pygame as py, constants, colors, math
+import pygame as py, constants, colors, math, algo, time, sys
 
 py.init()
 
 screen = py.display.set_mode(size=constants.SCREEN_SIZE)
 screen_width, screen_height = py.display.get_surface().get_size()
 clock = py.time.Clock()
+
+# globals
+city_states = {}
+cities = {}
+adj = []
 
 def get_rect_pos(mouse_pos):
     return (mouse_pos[0] - constants.RADIUS, mouse_pos[1] - constants.RADIUS)
@@ -29,7 +34,7 @@ def update_weights(cities):
     for city_1 in cities.keys():
         temp = []
         for city_2 in cities.keys():
-            temp.append(get_distance(city_1, city_2))
+            temp.append(int(get_distance(city_1, city_2)))
         adj.append(temp)
     return adj
 
@@ -40,14 +45,42 @@ def draw_cities(cities, city_states):
     for i, city in enumerate(list(cities.keys())):
         py.draw.circle(screen, city_states[i], city, constants.RADIUS)
 
-def main():
-    run = True
-    mouse_pos = ()
+def init_globals():
+    global city_states
+    global cities
+    global adj
+    city_states = {}
     cities = {}
     adj = []
+
+def update_screen(cities, city_states, call):
+    screen.fill(colors.BLACK)
+    draw_cities(cities, city_states)
+
+    # cities_pos_list = list(cities.keys())
+    # for i in len(edges):
+    #     for j in len(edges):
+    #         if edges[i][j] == 1:
+    #             py.draw.line(screen,colors.WHITE, cities_pos_list[i], cities_pos_list[j], 2)
+
+
+    for city_1 in cities.keys():
+        for city_2 in cities.keys():
+            py.draw.line(screen,colors.WHITE, city_1, city_2, 2)
+    py.display.update()
+    if call:
+        py.time.delay(constants.WAIT)
+
+def main():
+    global city_states
+    global cities
+    global adj
+    run = True
+    mouse_pos = ()
     left_click = 0
     right_click = 0
-    city_states = {}
+    init_globals()
+    permute_call = False
     while run:
         clock.tick(constants.FPS)
         screen.fill(colors.BLACK)
@@ -66,6 +99,10 @@ def main():
                     delete_city(cities, mouse_rect)
                     adj = update_weights(cities)
 
+            elif event.type == py.KEYDOWN:
+                if event.key == py.K_d:
+                    permute_call = True
+
         mouse_state = py.mouse.get_pressed()
         left_click = mouse_state[0]
         right_click = mouse_state[2]
@@ -73,15 +110,20 @@ def main():
         for i in range(len(adj)):
             update_city_state(i, city_states, colors.RED)
 
-        draw_cities(cities, city_states)
+        update_screen(cities, city_states, permute_call)
 
-        for city_1 in cities.keys():
-            for city_2 in cities.keys():
-                py.draw.line(screen,colors.WHITE, city_1, city_2, 2)
-
-        #call rec
+        #call permute if len >= 5
+        if permute_call:
+            n = len(adj)
+            nodes = [x for x in range(n)]
+            algo.init_globals(city_states, cities, adj)
+            algo.permute(0, n, nodes, adj)
+            run = False
+            print(algo.mini)
         
-        py.display.update()
-
+        
 if __name__ == "__main__":
     main()
+    # unvisited = red
+    # visited = green
+    # current = purple
